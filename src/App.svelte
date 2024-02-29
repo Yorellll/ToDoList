838958967dab05ca29b729d0a3e7b740d5dbcbec
Alexandre-st <alexandre.saintprix@gmail.com>
2024-02-29 16:55:04 +0100

    Add creation of todos, as an array

--- src/App.svelte
+++ src/App.svelte
@@ -2,9 +2,11 @@
   import { Route, Router } from "svelte-routing";
   import Dashboard from "./lib/Dashboard.svelte";
   import FormListe from "./lib/FormListe.svelte";
+  import Hello from "./lib/Hello.svelte";
 </script>
 
 <Router>
   <Route path="/" component={Dashboard} />
+  <Route path="/:" component={Hello} />
   <Route path="/create" component={FormListe} />
 </Router>
