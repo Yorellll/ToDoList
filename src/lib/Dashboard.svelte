13517d10ca22a37e5ee79097c3ec7a04a9012368
JacqueVerc <jacquesvercucque@gmail.com>
2024-02-29 22:37:38 +0100

    add affichage de liste

--- src/lib/Dashboard.svelte
+++ src/lib/Dashboard.svelte
@@ -1,4 +1,9 @@
+<script lang="ts">
+    import Liste from "./Liste.svelte";
+</script>
+
 <section class="header container">
   <h1>TODO</h1>
   <a href="/create">Créer une Todo</a>
 </section>
+<Liste />
