61ae8741de9b5d42005abe51110cc636338d41f4
Alexandre-st <alexandre.saintprix@gmail.com>
2024-03-25 21:58:12 +0100

    Add button to archive

--- src/App.svelte
+++ src/App.svelte
@@ -1,14 +1,16 @@
 <script>
   import { Route, Router } from "svelte-routing";
+  import Header from "./layouts/Header.svelte";
   import Dashboard from "./lib/Dashboard.svelte";
-  import FormListe from "./lib/FormListe.svelte";
+  import FormList from "./lib/FormList.svelte";
   import CreateTodo from "./routes/CreateTodo.svelte";
 </script>
 
+<Header />
 <Router>
   <Route path="/" component={Dashboard} />
   <Route path="/:todo">
     <CreateTodo />
   </Route>
-  <Route path="/create" component={FormListe} />
+  <Route path="/create" component={FormList} />
 </Router>
--- src/lib/Liste.svelte
+++ src/lib/Liste.svelte
@@ -1,48 +1,69 @@
 <script lang="ts">
-    import type {typeListe} from "./FormListe.svelte";
-    import App from "../App.svelte";
-    // Le type des todos à été difini à la création, je le récupère juste
-    let listeTodos: typeListe[];
-    let listTask: String[];
+  import App from "../App.svelte";
+  import type { typeListe } from "./FormListe.svelte";
+  // Le type des todos à été difini à la création, je le récupère juste
+  let listeTodos: typeListe[];
+  let listTask: String[];
 
-    const getTodos = () => {
-        // Pour récupérer les todos
-        listeTodos = JSON.parse(localStorage.getItem("todosList") || "[]");
-        // console.log(listeTodos);
-    };
+  const getTodos = () => {
+    // Pour récupérer les todos
+    listeTodos = JSON.parse(localStorage.getItem("todosList") || "[]");
+    // console.log(listeTodos);
+  };
 
-    // Déclenchenement de la fonction getTodos
-    getTodos();
+  const archiveTodo = (todo: typeListe) => {
+    const archive = todo.archive;
 
+    // On change la valeur de archive
+    todo.archive = !archive;
+    console.log(todo);
+
+    localStorage.setItem("todosList", JSON.stringify(listeTodos));
+  };
+
+  // Déclenchenement de la fonction getTodos
+  getTodos();
 </script>
 
 <!-- <button on:click={getTodos}>Refresh</button> -->
 <section class="lists container">
-    <h2 class="big-title">Listes actives</h2>
-    <ul class="lists-content">
-        {#if listeTodos}
-            {#each listeTodos as todo}
-                <!--        Le const est ici car avec une simple "variable" = " ", elle s'affiche dans le dom-->
-                {@const callTask = listTask = todo["todos"]}
-                <li class="list">
-                    <a href="/{todo?.urlTitle}"><h3>{todo?.title}</h3></a>
-                    <button>Supprimer</button>
-                    <button>Archiver</button>
-                    {#if (listTask[0])}
-                        {#each {length: 2} as _, i}
-                            <li>
-                                <label for="did">{listTask[i]}</label>
-                                <!-- Retirer checkbox et mettre image -->
-                                <input type="checkbox" name="did">
-                            </li>
-                            {#if i === 1}
-                                <li>...</li>
-                            {/if}
-                        {/each}
-                    {/if}
-                </li>
+  <h2 class="big-title">Listes actives</h2>
+  <ul class="lists-content">
+    {#if listeTodos}
+      {#each listeTodos as todo}
+        <!--        Le const est ici car avec une simple "variable" = " ", elle s'affiche dans le dom-->
+        {@const callTask = listTask = todo["todos"]}
+        <li class="list">
+          <a href="/{todo?.urlTitle}"><h3>{todo?.title}</h3></a>
+          <button on:click={() => archiveTodo(todo)}>
+            <img
+              class="list-button"
+              src="/src/assets/archive.svg"
+              alt="Pour archiver"
+            />
+          </button>
+          {#if listTask[0]}
+            {#each { length: 2 } as _, i}
+              <li>
+                <label for="did">{listTask[i]}</label>
+                <!-- Retirer checkbox et mettre image -->
+                <input type="checkbox" name="did" />
+              </li>
+              {#if i === 1}
+                <li>...</li>
+              {/if}
             {/each}
-        {/if}
+          {/if}
+        </li>
+      {/each}
+    {/if}
+  </ul>
+</section>
 
-    </ul>
+<section class="archives container">
+  <h3 class="big-title">Listes archivées</h3>
+  <!-- {#if listeTodos}
+    {#each listeTodos as todo}{/each}
+  {/if} -->
+  <ul class="lists-content"></ul>
 </section>
