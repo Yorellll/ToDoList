8ccea2fca8e4086455d12bde525d10dc4113274a
Yorellll <leleoleroy@gmail.com>
2024-03-27 19:00:46 +0100

    Ajout de preview task + task details

--- src/lib/Liste.svelte
+++ src/lib/Liste.svelte
@@ -1,30 +1,48 @@
 <script lang="ts">
-  import type { typeListe } from "./FormListe.svelte";
-  // Le type des todos à été difini à la création, je le récupère juste
-  let listeTodos: typeListe[];
+    import type {typeListe} from "./FormListe.svelte";
+    import App from "../App.svelte";
+    // Le type des todos à été difini à la création, je le récupère juste
+    let listeTodos: typeListe[];
+    let listTask: String[];
 
-  const getTodos = () => {
-    // Pour récupérer les todos
-    listeTodos = JSON.parse(localStorage.getItem("todosList") || "[]");
-    // console.log(listeTodos);
-  };
+    const getTodos = () => {
+        // Pour récupérer les todos
+        listeTodos = JSON.parse(localStorage.getItem("todosList") || "[]");
+        // console.log(listeTodos);
+    };
 
-  // Déclenchenement de la fonction getTodos
-  getTodos();
+    // Déclenchenement de la fonction getTodos
+    getTodos();
+
 </script>
 
 <!-- <button on:click={getTodos}>Refresh</button> -->
 <section class="lists container">
-  <h2 class="big-title">Listes actives</h2>
-  <ul class="lists-content">
-    {#if listeTodos}
-      {#each listeTodos as todo}
-        <li class="list">
-          <a href="/{todo?.urlTitle}"><h3>{todo?.title}</h3></a>
-          <button>Supprimer</button>
-          <button>Archiver</button>
-        </li>
-      {/each}
-    {/if}
-  </ul>
+    <h2 class="big-title">Listes actives</h2>
+    <ul class="lists-content">
+        {#if listeTodos}
+            {#each listeTodos as todo}
+                <!--        Le const est ici car avec une simple "variable" = " ", elle s'affiche dans le dom-->
+                {@const callTask = listTask = todo["todos"]}
+                <li class="list">
+                    <a href="/{todo?.urlTitle}"><h3>{todo?.title}</h3></a>
+                    <button>Supprimer</button>
+                    <button>Archiver</button>
+                    {#if (listTask[0])}
+                        {#each {length: 2} as _, i}
+                            <li>
+                                <label for="did">{listTask[i]}</label>
+                                <!-- Retirer checkbox et mettre image -->
+                                <input type="checkbox" name="did">
+                            </li>
+                            {#if i === 1}
+                                <li>...</li>
+                            {/if}
+                        {/each}
+                    {/if}
+                </li>
+            {/each}
+        {/if}
+
+    </ul>
 </section>
--- src/routes/CreateTodo.svelte
+++ src/routes/CreateTodo.svelte
@@ -1,44 +1,77 @@
 <script lang="ts">
-  import type { typeListe } from "../lib/FormListe.svelte";
+    import type {typeListe} from "../lib/FormListe.svelte";
 
-  // Type pour la lsite à afficher
-  let todos: typeListe[];
+    // Type pour la lsite à afficher
+    let todos: typeListe[];
 
-  // Définition des variables initiales
-  let todoTitle = "";
+    // Définition des variables initiales
+    let todoTitle = "";
 
-  // Récupération de l'url en la formattant pour récupérer le nom de la liste
-  const location = document.location.pathname.split("/")[1];
+    // Récupération de l'url en la formattant pour récupérer le nom de la liste
+    const location = document.location.pathname.split("/")[1];
 
-  // Pour récupérer les todos
-  todos = JSON.parse(localStorage.getItem("todosList") || "[]");
-  // console.log(todos);
+    // Pour récupérer les todos
+    todos = JSON.parse(localStorage.getItem("todosList") || "[]");
+    // console.log(todos);
 
-  // Stock et récupère la liste à afficher qui correspond à mon url
-  let todoToShow = todos.find((todo) => todo.urlTitle === location);
+    // Stock et récupère la liste à afficher qui correspond à mon url
+    let todoToShow = todos.find((todo) => todo.urlTitle === location);
 
-  console.log(todoToShow);
+    console.log(todoToShow);
 
-  const addTodo = () => {
-    // Ajout de la nouvelle tâche dans la liste todos
-    todoToShow?.todos.push(todoTitle);
-    // Mise à jour de la liste dans le localStorage
-    localStorage.setItem("todosList", JSON.stringify(todos));
-  };
+    const addTodo = () => {
+        if (todoTitle) {
+            // Ajout de la nouvelle tâche dans la liste todos
+            todoToShow?.todos.push(todoTitle);
+            // Mise à jour de la liste dans le localStorage
+            localStorage.setItem("todosList", JSON.stringify(todos));
+            todos = JSON.parse(localStorage.getItem("todosList") || "[]");
+            todoToShow = todos.find((todo) => todo.urlTitle === location);
+            todoTitle = "";
+        }
+    };
+
+    let check = true;
+    let checkboxStyle = "";
+
+    //class css activer en fonction du check ou non de la checkbox +ajouter un bool dans la liste
+    //de task afin d'enregistrer en session storage l'état de chaque checkbox
+
+
 </script>
 
 <div class=" create container">
-  {#if todoToShow}
-    <h1 class="big-title">{todoToShow.title}</h1>
-    <div class="create-input">
-      <label for="title">Nom de la liste</label>
-      <input
-        id="title"
-        type="text"
-        bind:value={todoTitle}
-        placeholder="Repas, Achat Vélo, Gateau au chocolat..."
-      />
-      <button class="btn btn-header" on:click={addTodo}>Créer</button>
-    </div>
-  {/if}
+    {#if todoToShow}
+        <h1 class="big-title">{todoToShow.title}</h1>
+        <div class="create-input">
+            <label for="title">Nom de la liste</label>
+            <input
+                    id="title"
+                    type="text"
+                    bind:value={todoTitle}
+                    placeholder="Repas, Achat Vélo, Gateau au chocolat..."
+            />
+            <button class="btn btn-header" on:click={addTodo}>Créer</button>
+
+        </div>
+    {/if}
 </div>
+
+{#if (todoToShow && todoToShow.todos)}
+    <div class="  task container">
+        {#each todoToShow.todos as task}
+            <li class="list">
+                <label class="nameTask" for="did">{task}</label>
+                <input
+                       id="check" type="checkbox"
+                       name="did"
+                       bind:checked={check}>
+                <!--{#if (check)}-->
+                <!--    {@const checkboxStyle = "text-decoration: line-through;"}-->
+                <!--{:else}-->
+                <!--    {@const checkboxStyle = ""}-->
+                <!--{/if}-->
+            </li>
+        {/each}
+    </div>
+{/if}
