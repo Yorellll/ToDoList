38426f698def60eb0434bbef63355ff548357e0d
Yorellll <leleoleroy@gmail.com>
2024-03-28 00:29:14 +0100

    Ajout de la fonction achieved

--- src/routes/CreateTodo.svelte
+++ src/routes/CreateTodo.svelte
@@ -1,11 +1,14 @@
 <script lang="ts">
-    import type {typeListe} from "../lib/FormListe.svelte";
+    import type { taskType, typeListe} from "../lib/FormListe.svelte";
 
     // Type pour la lsite à afficher
     let todos: typeListe[];
 
-    // Définition des variables initiales
-    let todoTitle = "";
+    //Task courante que l'on ajoute, du type taskType ajouté dans FormListe
+    const todoTitle: taskType = {
+        task:"",
+        check: false
+    }
 
     // Récupération de l'url en la formattant pour récupérer le nom de la liste
     const location = document.location.pathname.split("/")[1];
@@ -17,25 +20,36 @@
     // Stock et récupère la liste à afficher qui correspond à mon url
     let todoToShow = todos.find((todo) => todo.urlTitle === location);
 
+
     console.log(todoToShow);
 
     const addTodo = () => {
-        if (todoTitle) {
+        if (todoTitle.task) {
             // Ajout de la nouvelle tâche dans la liste todos
             todoToShow?.todos.push(todoTitle);
             // Mise à jour de la liste dans le localStorage
             localStorage.setItem("todosList", JSON.stringify(todos));
             todos = JSON.parse(localStorage.getItem("todosList") || "[]");
             todoToShow = todos.find((todo) => todo.urlTitle === location);
-            todoTitle = "";
+            todoTitle.task = "";
         }
     };
 
-    let check = true;
-    let checkboxStyle = "";
 
-    //class css activer en fonction du check ou non de la checkbox +ajouter un bool dans la liste
-    //de task afin d'enregistrer en session storage l'état de chaque checkbox
+    //Cette fonction est appelé à l'update du check, elle met a jour son état et la met à jour dans le stockage
+    const changeCheckState = (  nameTask: string,state:boolean) => {
+        if (todoToShow){
+            todoToShow.todos.forEach(function (task){
+                if (task.task == nameTask){
+                    task.check = state;
+                    localStorage.setItem("todosList", JSON.stringify(todos));
+                    todos = JSON.parse(localStorage.getItem("todosList") || "[]");
+                    todoToShow = todos.find((todo) => todo.urlTitle === location);
+                }
+
+            })
+        }
+    }
 
 
 </script>
@@ -48,7 +62,7 @@
             <input
                     id="title"
                     type="text"
-                    bind:value={todoTitle}
+                    bind:value={todoTitle.task}
                     placeholder="Repas, Achat Vélo, Gateau au chocolat..."
             />
             <button class="btn btn-header" on:click={addTodo}>Créer</button>
@@ -59,18 +73,15 @@
 
 {#if (todoToShow && todoToShow.todos)}
     <div class="  task container">
-        {#each todoToShow.todos as task}
+        {#each todoToShow.todos as taskCourante}
             <li class="list">
-                <label class="nameTask" for="did">{task}</label>
+                <label class="nameTask" for="did" class:achievedTask={taskCourante.check}>{taskCourante.task}</label>
                 <input
                        id="check" type="checkbox"
                        name="did"
-                       bind:checked={check}>
-                <!--{#if (check)}-->
-                <!--    {@const checkboxStyle = "text-decoration: line-through;"}-->
-                <!--{:else}-->
-                <!--    {@const checkboxStyle = ""}-->
-                <!--{/if}-->
+                       bind:checked={taskCourante.check}
+                       on:change={() => changeCheckState(taskCourante.task, taskCourante.check)}
+                       >
             </li>
         {/each}
     </div>
