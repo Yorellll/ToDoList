9e48c58cc7540cc8a6f7928a61a2a5e6a88ded02
Alexandre-st <alexandre.saintprix@gmail.com>
2024-03-28 16:17:00 +0100

    Add a fix for preview of a todo

--- src/lib/Liste.svelte
+++ src/lib/Liste.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
   import App from "../App.svelte";
-  import type { typeListe } from "./FormListe.svelte";
+  import type { typeListe } from "./FormList.svelte";
   // Le type des todos à été difini à la création, je le récupère juste
   let listeTodos: typeListe[];
   let listTask: String[];
@@ -45,6 +45,18 @@
                 alt="Pour archiver"
               />
             </button>
+            {#each { length: 2 } as _, i}
+              {#if todo["todos"][i]}
+                <div>
+                  <label for="did">{todo["todos"][i]["task"]}</label>
+                  <!-- Retirer checkbox et mettre image -->
+                  <input type="checkbox" name="did" />
+                </div>
+                {#if i === 1}
+                  <p>...</p>
+                {/if}
+              {/if}
+            {/each}
           </li>
         {/if}
       {/each}
@@ -57,8 +69,6 @@
   <ul role="list" class="lists-content">
     {#each listeTodos as todo}
       {#if todo.archive === true}
-        <!--        Le const est ici car avec une simple "variable" = " ", elle s'affiche dans le dom-->
-        {@const callTask = listTask = todo["todos"]}
         <li class="list">
           <a href="/{todo?.urlTitle}"><h3>{todo?.title}</h3></a>
           <button on:click={() => archiveTodo(todo)}>
@@ -68,18 +78,6 @@
               alt="Pour archiver"
             />
           </button>
-          {#if listTask[0]}
-            {#each { length: 2 } as _, i}
-              <li>
-                <label for="did">{listTask[i]}</label>
-                <!-- Retirer checkbox et mettre image -->
-                <input type="checkbox" name="did" />
-              </li>
-              {#if i === 1}
-                <li>...</li>
-              {/if}
-            {/each}
-          {/if}
         </li>
       {/if}
     {/each}
