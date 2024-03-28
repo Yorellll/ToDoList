b0807e82905bf5edeb74faa347f6f074800d183e
JacqueVerc <jacquesvercucque@gmail.com>
2024-03-28 20:14:51 +0100

    Ajout des sous-listes

--- src/App.svelte
+++ src/App.svelte
@@ -1,16 +1,19 @@
 <script>
-  import { Route, Router } from "svelte-routing";
-  import Header from "./layouts/Header.svelte";
-  import Dashboard from "./lib/Dashboard.svelte";
-  import FormList from "./lib/FormList.svelte";
-  import CreateTodo from "./routes/CreateTodo.svelte";
+    import {Route, Router} from "svelte-routing";
+    import Header from "./layouts/Header.svelte";
+    import Dashboard from "./lib/Dashboard.svelte";
+    import FormList from "./lib/FormList.svelte";
+    import CreateTodo from "./routes/CreateTodo.svelte";
 </script>
 
-<Header />
+<Header/>
 <Router>
-  <Route path="/" component={Dashboard} />
-  <Route path="/:todo">
-    <CreateTodo />
-  </Route>
-  <Route path="/create" component={FormList} />
+    <Route path="/" component={Dashboard}/>
+    <Route path="/:todo">
+        <CreateTodo/>
+    </Route>
+    <Route path="/:todo/:subTodo">
+        <CreateTodo/>
+    </Route>
+    <Route path="/create" component={FormList}/>
 </Router>
