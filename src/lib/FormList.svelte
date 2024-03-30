da6f661f1b19eae9cb80a1265f2111e2d6d5015f
Alexandre-st <alexandre.saintprix@gmail.com>
2024-03-30 12:21:22 +0100

    Add a new regex and working perfectly

--- src/lib/FormList.svelte
+++ src/lib/FormList.svelte
@@ -24,6 +24,12 @@
     localStorage.getItem("todosList") || "[]"
   );
 
+  // Pour retirer les accents et caractères spéciaux
+  export const withoutAccent = (str: string) => {
+    return str.normalize("NFD").replace(/[\u0300-\u036f]/g, "");
+  };
+
+  // Regex pour avoir une url propre
   export const pattern = /[^a-zA-Z0-9\u00C0-\u017F]+/g;
 
   // Création d'une liste et récupération de son nom
@@ -31,7 +37,7 @@
     // Définition de la liste
     const list = {
       title: title,
-      urlTitle: title.replace(pattern, "-").toLowerCase(),
+      urlTitle: withoutAccent(title.replace(pattern, "-").toLowerCase()),
       archive: false,
       todos: [],
       subLists: [],
@@ -42,11 +48,9 @@
     // Et on rajoute à todos la liste créée
     todos = [...todos, list];
     localStorage.setItem("todosList", JSON.stringify(todos));
-    console.log(title.replace(pattern, "-").toLowerCase());
 
     // Redirection vers la liste créée
-    // .tolowerCase() pour éviter les problèmes de casse
-    navigate(`/${title.replace(pattern, "-").toLowerCase()}`);
+    navigate(`/${withoutAccent(title.replace(pattern, "-").toLowerCase())}`);
   };
 </script>
 
@@ -63,8 +67,3 @@
     <button class="btn btn-header" on:click={redirectAndCreate}>Créer</button>
   </div>
 </section>
-<!-- Pas besoin de générer un page.ts et +page.svelteHTML
-Mettre son dossier dans un dossier [todo]
-faire le fichier +page.svelte
-Récupérer le params de title
--->
--- src/routes/CreateTodo.svelte
+++ src/routes/CreateTodo.svelte
@@ -1,13 +1,13 @@
 <script lang="ts">
   import {
     pattern,
+    withoutAccent,
     type taskType,
-    type typeListe,
+    type typeListe
   } from "../lib/FormList.svelte";
 
   // Type pour la lsite à afficher
   let todos: typeListe[];
-  //   let todoToShow: typeListe | null;
 
   //Task courante que l'on ajoute, du type taskType ajouté dans FormListe
   const todoTitle: taskType = {
@@ -34,8 +34,7 @@
         // Crée une sous liste
         let subList: typeListe = {
           title: todoTitle.task,
-          urlTitle:
-            currentTodo.urlTitle + "/" + todoTitle.task.replace(pattern, "-"),
+          urlTitle: currentTodo.urlTitle + "/" + withoutAccent(todoTitle.task.replace(pattern, "-").toLowerCase()),
           archive: false,
           todos: [],
           subLists: [],
@@ -53,7 +52,7 @@
       todos = JSON.parse(localStorage.getItem("todosList") || "[]");
       if (subLocation) {
         todoToShow.subLists.forEach((sub: typeListe) => {
-          if (sub.title.replace(pattern, "-") === subLocation) {
+          if (withoutAccent(sub.title.replace(pattern, "-").toLowerCase()) === subLocation) {
             todoToShow = sub;
           }
         });
@@ -72,7 +71,7 @@
           todos = JSON.parse(localStorage.getItem("todosList") || "[]");
           if (subLocation) {
             todoToShow.subLists.forEach((sub: typeListe) => {
-              if (sub.title.replace(pattern, "-") === subLocation) {
+              if (withoutAccent(sub.title.replace(pattern, "-").toLowerCase()) === subLocation) {
                 todoToShow = sub;
               }
             });
@@ -86,7 +85,7 @@
 <div class=" create container">
   {#if todoToShow && subLocation}
     {#each todoToShow.subLists as lists}
-      {#if lists.title.replace(pattern, "-") === subLocation}
+      {#if withoutAccent(lists.title.replace(pattern, "-").toLowerCase()) === subLocation}
         <h1 class="big-title">{lists.title}</h1>
         <div class="create-input">
           <label for="title">Nom de la liste</label>
@@ -96,34 +95,21 @@
             bind:value={todoTitle.task}
             placeholder="Repas, Achat Vélo, Gateau au chocolat..."
           />
-          <button class="btn btn-header" on:click={() => addTodo(lists, false)}>
-            Créer
-          </button>
+          <button class="btn btn-header" on:click={() => addTodo(lists, false)}> Créer </button>
           {#if !subLocation}
-            <button
-              class="btn btn-header"
-              on:click={() => addTodo(lists, true)}
-            >
-              Créer une liste secondaire
-            </button>
+            <button class="btn btn-header" on:click={() => addTodo(lists, true)}> Créer une liste secondaire </button>
           {/if}
         </div>
         <div class="container">
           {#each lists.todos as taskCourante}
             <div class="task">
-              <label
-                class="nameTask"
-                for="did"
-                class:achievedTask={taskCourante.check}
-                >{taskCourante.task}</label
-              >
+              <label class="nameTask" for="did" class:achievedTask={taskCourante.check}>{taskCourante.task}</label>
               <input
                 id="check"
                 type="checkbox"
                 name="did"
                 bind:checked={taskCourante.check}
-                on:change={() =>
-                  changeCheckState(taskCourante.task, taskCourante.check)}
+                on:change={() => changeCheckState(taskCourante.task, taskCourante.check)}
               />
             </div>
           {/each}
@@ -141,15 +127,9 @@
         bind:value={todoTitle.task}
         placeholder="Repas, Achat Vélo, Gateau au chocolat..."
       />
-      <button class="btn btn-header" on:click={() => addTodo(todoToShow, false)}
-        >Créer</button
-      >
+      <button class="btn btn-header" on:click={() => addTodo(todoToShow, false)}>Créer</button>
       {#if !subLocation}
-        <button
-          class="btn btn-header"
-          on:click={() => addTodo(todoToShow, true)}
-          >Créer une liste secondaire</button
-        >
+        <button class="btn btn-header" on:click={() => addTodo(todoToShow, true)}>Créer une liste secondaire</button>
       {/if}
     </div>
   {/if}
@@ -161,11 +141,8 @@
       <h2>Vos listes secondaires</h2>
       {#each todoToShow.subLists as currentList}
         <div class="list">
-          <a
-            aria-label={`Lien vers ${currentList.title}`}
-            href="/{currentList?.urlTitle}"
-          >
-            <h3>{currentList?.title}</h3>
+          <a aria-label={`Lien vers ${currentList.title}`} href="/{currentList.urlTitle}">
+            <h3>{currentList.title}</h3>
           </a>
         </div>
       {/each}
@@ -174,18 +151,13 @@
   <div class="container">
     {#each todoToShow.todos as taskCourante}
       <li class:achieve={taskCourante.check} class="task">
-        <label
-          class="nameTask"
-          for="did"
-          class:achievedTask={taskCourante.check}>{taskCourante.task}</label
-        >
+        <label class="nameTask" for="did" class:achievedTask={taskCourante.check}>{taskCourante.task}</label>
         <input
           id="check"
           type="checkbox"
           name="did"
           bind:checked={taskCourante.check}
-          on:change={() =>
-            changeCheckState(taskCourante.task, taskCourante.check)}
+          on:change={() => changeCheckState(taskCourante.task, taskCourante.check)}
         />
       </li>
     {/each}
