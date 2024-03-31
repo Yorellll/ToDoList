88c70c72ed2be17121096364c5f36ab6e1acedd2
JacqueVerc <jacquesvercucque@gmail.com>
2024-03-31 15:08:40 +0200

    Ajout breadcrumb

---
+++ src/lib/BreadCrumb.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+
+    import type {typeListe} from "./FormList.svelte";
+
+    export let previousList: typeListe;
+    export let currentList: typeListe;
+</script>
+<p class="breadcrumb-content">
+    <a aria-label={`Lien vers ${previousList.title}`} href="/{previousList.urlTitle}">{previousList.title}</a>
+    /
+    <a aria-label={`Lien vers ${currentList.title}`} href="/{currentList.urlTitle}">{currentList.title}</a>
+</p>
