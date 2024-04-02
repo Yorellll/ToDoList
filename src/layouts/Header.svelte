7510e65c4289ee14f23c4e85b5d0b141070c4868
Yorellll <leleoleroy@gmail.com>
2024-04-02 23:56:15 +0200

    suppression /

--- src/layouts/Header.svelte
+++ src/layouts/Header.svelte
@@ -1,6 +1,6 @@
 <header class="header container">
-  <a href="/">
+  <a href="">
     <img class="header-img" src="/src/assets/TODOList.jpg" alt="Create your own todolist">
   </a>
-  <a class="btn btn-header" href="/create">Créer une Todo</a>
+  <a class="btn btn-header" href="create">Créer une Todo</a>
 </header>
